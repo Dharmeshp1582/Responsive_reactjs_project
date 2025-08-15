dd9fa6c69f68e8160501d332a2964c0da1f29fbe
Dharmeshp1582 <alpeshpatelvirpur@gmail.com>
2025-08-15 12:20:55 +0530

    add framer motion

--- Agency.Ai/src/components/Services.jsx
+++ Agency.Ai/src/components/Services.jsx
@@ -1,48 +1,66 @@
-import React from 'react'
-import assets from '../assets/assets'
-import Title from './Title'
-import ServiceCard from './ServiceCard'
+import React from "react";
+import assets from "../assets/assets";
+import Title from "./Title";
+import ServiceCard from "./ServiceCard";
+import { motion } from "motion/react";
 
 const Services = () => {
-
- const servicesData = [
-  {
-    title: 'Advertising',
-    description : 'We turn bold ideas into powerful digital campaigns, helping brands connect with their audience and drive results.',
-    icon: assets.ads_icon
-  },
-  {
-    title: 'Content Marketing',
-    description: 'We help you execute your plan and deliver results. We have a team of experts who can help you create and execute your content marketing strategy.',
-    icon: assets.marketing_icon
-  },
-  {
-    title: 'Content Writing',
-    description : 'We help you create a marketing strategy that drives results.',
-    icon: assets.content_icon
-  },
-  {
-    title: 'Social Media',
-    description : 'We help you builds a strong social media presence and engage with your audience.',
-    icon: assets.social_icon
-  }
- ]
+  const servicesData = [
+    {
+      title: "Advertising",
+      description:
+        "We turn bold ideas into powerful digital campaigns, helping brands connect with their audience and drive results.",
+      icon: assets.ads_icon,
+    },
+    {
+      title: "Content Marketing",
+      description:
+        "We help you execute your plan and deliver results. We have a team of experts who can help you create and execute your content marketing strategy.",
+      icon: assets.marketing_icon,
+    },
+    {
+      title: "Content Writing",
+      description:
+        "We help you create a marketing strategy that drives results.",
+      icon: assets.content_icon,
+    },
+    {
+      title: "Social Media",
+      description:
+        "We help you builds a strong social media presence and engage with your audience.",
+      icon: assets.social_icon,
+    },
+  ];
 
   return (
-    <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
-  
-  <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden' />
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      transition={{
+        staggerChildren: 0.2,
+      }}
+      viewport={{ once: true }}
+      id="services"
+      className="relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
+    >
+      <img
+        src={assets.bgImage2}
+        alt=""
+        className="absolute -top-110 -left-70 -z-1 dark:hidden"
+      />
 
-  <Title title='How can we help?' description='From strategy to execution, We craft digital solutions that move your business forward.  '/>
+      <Title
+        title="How can we help?"
+        description="From strategy to execution, We craft digital solutions that move your business forward.  "
+      />
 
-  <div className='flex flex-col md:grid grid-cols-2'>
-    {servicesData.map((service, index) => (
-      <ServiceCard key={index} service={service} index={index} />
-    ))}
-  </div>
+      <div className="flex flex-col md:grid grid-cols-2">
+        {servicesData.map((service, index) => (
+          <ServiceCard key={index} service={service} index={index} />
+        ))}
+      </div>
+    </motion.div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Services
+export default Services;
